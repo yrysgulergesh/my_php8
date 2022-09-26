e0734b1e46eccc85f72a006fe2ed50a6ef452622
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-09-26 16:07:36 +0600

    init

---
+++ html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>START PHP 8</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<h1>
+<?php
+echo "Первый сайт на PHP";
+?>
+</h1>
+<div>
+<?php
+echo "<h2>Заголовок параграфа</h2>";
+echo "Текст параграфа";
+?>
+</div>
+</body>
+</html>
